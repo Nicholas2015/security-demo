83560193615c1e86054c9686bc2be162cddffc71
Nicholas <799327210@qq.com>
2022-10-06 21:38:31 +0800

    userdetails学习

---
+++ src/main/java/com/example/securitydemo/provider/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,34 @@
+package com.example.securitydemo.provider;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+
+/**
+ * @author Nicholas Sun
+ * @date 2022/10/06 21:17
+ */
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Optional<? extends GrantedAuthority> authority = authorities.stream().filter(a -> a.getAuthority().equals("read")).findFirst();
+        if (authority.isPresent()){
+            response.sendRedirect("/home");
+        } else {
+            response.sendRedirect("/error");
+        }
+    }
+}
