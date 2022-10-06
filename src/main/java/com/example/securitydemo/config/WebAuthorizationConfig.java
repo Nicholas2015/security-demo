5c11fc69faf3027facc60c42c293177911c6c119
Nicholas <799327210@qq.com>
2022-10-06 21:04:52 +0800

    userdetails学习

--- src/main/java/com/example/securitydemo/config/WebAuthorizationConfig.java
+++ src/main/java/com/example/securitydemo/config/WebAuthorizationConfig.java
@@ -14,7 +14,7 @@ public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.httpBasic();
-        http.formLogin();
+        http.formLogin().defaultSuccessUrl("/home",true);
         http.authorizeRequests().anyRequest().authenticated();
     }
 }
