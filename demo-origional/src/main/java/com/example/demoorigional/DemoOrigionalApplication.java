6bcc276ea1400e3989bc1c591c53ad2924992966
Nicholas <799327210@qq.com>
2022-10-08 22:10:43 +0800

    spring security demo01

---
+++ demo-origional/src/main/java/com/example/demoorigional/DemoOrigionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoorigional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOrigionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOrigionalApplication.class, args);
+    }
+
+}
---
+++ demo-origional/src/test/java/com/example/demoorigional/DemoOrigionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoorigional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOrigionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
