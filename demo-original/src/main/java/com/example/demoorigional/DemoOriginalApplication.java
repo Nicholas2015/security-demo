fbec8e482b49e71837e66d92b2ad69a921825a00
Nicholas <799327210@qq.com>
2022-10-08 22:12:46 +0800

    spring security demo01

--- demo-original/src/main/java/com/example/demoorigional/DemoOriginalApplication.java
+++ demo-original/src/main/java/com/example/demoorigional/DemoOriginalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoOrigionalApplication {
+public class DemoOriginalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoOrigionalApplication.class, args);
+        SpringApplication.run(DemoOriginalApplication.class, args);
     }
 
 }
