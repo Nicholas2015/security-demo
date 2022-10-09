e0675c679e0dfea992b4c81afd01bdf3191894b2
Nicholas <799327210@qq.com>
2022-10-09 20:11:16 +0800

    spring security demo01

--- demo-original/src/main/java/com/example/demoorigional/filter/CustomAuthenticationProvider1.java
+++
@@ -1,1 +0,0 @@
-package com.example.demoorigional.filter;
--- demo-original/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO `spring`.`authorities` VALUES (NULL,'nicholas','write');
-INSERT IGNORE INTO `spring`.`users` VALUES (NULL,'nicholas','123456','1');
